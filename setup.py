a133a1feaf2e602d8fb3f8a122aafe68caaa9af5
Cubiss <solin.jakub@gmail.com>
2020-05-16 11:50:56 +0200

    Added __init__.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 packages = []
 
-setup(name='c__lib',
+setup(name='sendmail',
       version='1.0',
       description='Simple email sending via already working SMTP server without authentication.',
       url='https://github.com/Cubiss/sendmail',
       author='Cubiss',
       author_email='cubiss.dev@gmail.com',
       license='WTFPL',
-      packages=['c__lib'],
+      packages=['sendmail'],
       zip_safe=True,
       install_requires=packages)
